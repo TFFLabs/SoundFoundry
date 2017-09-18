3fc9f85c8ba14e6b7b739eb4bc26a4edf5d8dc46
Edgar Garcia <Edgar.garcia@chromeriver.com>
2017-09-18 10:01:38 -0500

    #46 [Adding color to the avatar images]
    Story: https://github.com/TFFLabs/SoundFoundry/issues/46
    
    So that it is easy to identify different users

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,5 @@
-import { Image } from "app/models/image";
-import { Serializable } from "app/models/serializable";
+import { Image } from 'app/models/image';
+import { Serializable } from 'app/models/serializable';
 
 export class User implements Serializable<User> {
   country: string;
@@ -13,23 +13,39 @@ export class User implements Serializable<User> {
     if (input) {
       this.id = input.id;
       this.country = input.country;
-      this.display_name = input.display_name? input.display_name : this.id;
+      this.display_name = input.display_name ? input.display_name : this.id;
       this.email = input.email;
       this.images = input.images.map(value => new Image().deserialize(value));
-      if(!this.images || this.images.length == 0){
-        let img = new Image();
-        img.url = "../../assets/img/boy-with-headphones.png";
+      if (!this.images || this.images.length === 0) {
+        const img = new Image();
+        img.url = '../../assets/img/boy-with-headphones.png';
         this.images.push(img);
       }
       this.thumbnail_small = this.getSmallestImage();
     }
-    
+
     return this;
   }
 
-  private getSmallestImage(): Image {
-    if(this.images && this.images.length > 0){
-        return this.images.sort((image1, image2) => image1.width - image2.width)[0];
+  public getSmallestImage(): Image {
+    if (this.images && this.images.length > 0) {
+      return this.images.sort(
+        (image1, image2) => image1.width - image2.width
+      )[0];
     }
+  }
+
+  // gets a color based on the user display name, the linting error here is a bitwise one.
+  public getVoterColor(): string {
+    let hash = 0;
+    for (let i = 0; i < this.display_name.length; i++) {
+      hash = this.display_name.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let colour = '#';
+    for (let i = 0; i < 3; i++) {
+      const value = (hash >> (i * 8)) & 0xFF;
+      colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
   }
 }
